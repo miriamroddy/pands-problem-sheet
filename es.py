c72eacdab75feb02785f2d6dc099b9fb9b72256b
unknown <Administrator@LTCFAPW00B3WL.healthirl.net>
2023-04-19 02:50:43 +0100

    Es Week 7 addition

--- es.py
+++ es.py
@@ -1,3 +1,12 @@
 import sys
 
 filename = sys.argv[1]
+with open(filename, 'r') as f:
+    contents = f.read()
+
+count = 0
+for letter in contents:
+    if letter == 'e':
+        count += 1
+
+print(count)

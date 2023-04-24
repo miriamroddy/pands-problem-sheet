6e36ce7b4ffe0e4627f4b1123d492b48865f7fc7
miriamroddy <98190341+miriamroddy@users.noreply.github.com>
2023-04-24 11:00:14 +0100

    Adding extra accounts file

---
+++ 03.02accounts.py
@@ -0,0 +1,31 @@
+## Write a python program called accounts.py that reads in a 10 character account number and outputs the account number 
+## with only the last 4 digits showing (and the first 6 digits replaced with Xs). Extra: Modify the program to deal with account numbers of any length 
+## (yes that is a vague requirement; comment your assumptions).
+
+##  The user input is stored in the account_number variable. We explicitly state that the account number can be up to 34 characters -
+##  In practice, the code can actually cope with longer numbers. I chose to go with 34 characters because this is the maximum length of SEPA account numbers. 
+
+
+account_number = input("Enter your account number (up to 34 characters): ")
+
+## The program uses the slice function to extract the last 4 digits of the account number, and stores them in a variable called last_4_digits.
+
+last_4_digits = account_number[-4:]
+
+## Python calculates the number of Xs required by subtracting 4 (the number of last digits to be shown) from the length of the account number. Even though SEPA accounts can be no longer
+#  than 34 characters, this can actually cope with larger numbers.This result is then stored in the num_of_xs variable.
+
+number_of_xs = len(account_number) - 4
+
+## The program creates a string called number_of_xs and stores it in a variable called xs. number_of_xs. 
+## This will output how many Xs need to go before the last four digits of the account number. 
+
+XString = "X" * number_of_xs
+
+## We now concatenate the xs string and the last_4_digits string to create a masked account number , which we store in masked_account_number:
+
+masked_account_number = XString + last_4_digits
+
+## Finally, we print the masked account number to the console:
+
+print("Your account number is:", masked_account_number)

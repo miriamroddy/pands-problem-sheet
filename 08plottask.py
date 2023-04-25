2966db71c7b0ba419e36de4f143938dc33d436db
miriamroddy <98190341+miriamroddy@users.noreply.github.com>
2023-04-25 12:15:17 +0100

    Update 08plottask.py - adding spaces for readability

--- 08plottask.py
+++ 08plottask.py
@@ -3,12 +3,14 @@
 ## and a plot of the function  h(x)=x3 in the range [0, 10], on the one set of axes. Some marks will be given for making the plot look nice (legend etc).
 
 ## We import the numpy library and give it the alias np (for ease of reference) and do the same with the matplotlib library, giving it the alias plt.
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 ## We generate 1000 random values from a normal distro with mean 5 and SD of 2. The generated values are stored in the data variable - we want an array that contains 1000
 ## random numbers with a mean of 5 and an SD of 2.
+
 data = np.random.normal(5, 2, 1000)
 
 ## Now that we have the 1000 values we want, we want to create a histogram of what we have stored in the data variable.  Data refers to the array we just created. Bins=30 specifies
@@ -22,17 +24,23 @@ plt.hist(data, bins=30, alpha=0.5, label='Normal Distribution')
 x = np.linspace(0, 10, 100)
 
 # We calculate the function h(x) = x^3 for each value of x
+
 y = x ** 3
 
 # Plot the function h(x) on the same graph with a red label - the plt is an alias for the mathplotlib library
+
 plt.plot(x, y, color='red', label='h(x) = x^3')
 
 # Add labels to both axes
+
 plt.xlabel('X')
 plt.ylabel('Frequency / h(x)')
+
 ## We want to give the graph a title and to include a legend
+
 plt.title('Task 8: Histogram of Normal Distribution and Plot of h(x) = x^3')
 plt.legend()
 
 # Finally, we show the resulting graph
+
 plt.show()

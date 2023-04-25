a798611960e011db65cdf30a4e522ff08a651426
miriamroddy <98190341+miriamroddy@users.noreply.github.com>
2023-04-25 12:12:06 +0100

    Update 07es.py - adding spaces for readability

--- 07es.py
+++ 07es.py
@@ -2,7 +2,6 @@
 ## Think about what is being asked here, document any assumptions you are making.
 ## The program should take the filename from an argument on the command line.
 
-
 ## We start by importing the sys module
 
 import sys
@@ -24,10 +23,13 @@ filename = sys.argv[1]
 with open(filename, "r") as file:
 
 ## Once the file is opened, the program iterates over each line of the files using a for loop. For every line, the code counts how many times the characters 'e' and 'E' appear 
-# in the line using the count() method of the string object. It then adds the count of each line to a running total count. When the loop finishes, what is stored in count is the total
+## in the line using the count() method of the string object. It then adds the count of each line to a running total count. When the loop finishes, what is stored in count is the total
 ## number of occurances of both e and E in the file.
+
     count = 0
     for line in file:
         count += line.count('e') + line.count('E')
+        
  ## we use an f-string to print the number of occurances to the console.
+
 print(f"The file {filename} contains {count} e's.")

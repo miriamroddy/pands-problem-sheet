78a34f7c4f8f230388d2b547aaa6b8f46bfeeb22
miriamroddy <98190341+miriamroddy@users.noreply.github.com>
2023-04-25 12:09:41 +0100

    Update 03.01accounts.py - adding spaces for readability

--- 03.01accounts.py
+++ 03.01accounts.py
@@ -6,19 +6,30 @@
 ## enter their account number again until a valid input is received.
 
 ## We create an infinite while loop that will continue to prompt the user for input until a valid account number is entered.
+
 while True:
     
 ## We prompt the user to enter their account number and store is in the account_number variable:
+  
     account_number = input("Enter your 10-digit account number: ")
+    
 ## We use an if statement to check if the length (using len) of the account number is equal to ten digits:
+
     if len(account_number) == 10:
+        
 ## We create a new variable (masked_account_number) by concatenating the first six characters with XXXXX and the last four characters or the account_number variable:
+
         masked_account_number = "XXXXXX" + account_number[-4:]
+    
 ## we use break to exit the loop when a valid account number has been entered
+
         break
+    
 ## If the account number is not ten digits long, an error message is printed to the console:
+
     else:
         print("Error: account number must be 10 digits.")
 
 # Lastly, we print out the masked account number to the user
+
 print("Your masked account number is:", masked_account_number)
